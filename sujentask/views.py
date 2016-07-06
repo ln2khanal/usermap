419dae359268e93ed4d7e22be8630439aeff240c
Lekhnath Khanal <lekhnath.khanal@logpoint.com>
2016-07-06 11:21:03 +0545

    redirected site index to route app's index

--- sujentask/views.py
+++ sujentask/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 def index(request):
     # didn't create template for it.
-    return HttpResponse("<center>Working!<br/>Jump to /route/</center>")
+    return redirect('/route/')
