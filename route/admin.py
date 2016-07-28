b7be357ead925cfc49184f9bd49fb2a22e22aa8e
Lekhnath Khanal <lekhnath.khanal@logpoint.com>
2016-07-29 00:24:13 +0545

    User & Route models registered to admin module.

--- route/admin.py
+++ route/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from route.models import User, Route
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    empty_value_display = 'N/A'
+    list_display = ('first_name', 'last_name', 'user_key')
+    search_fields = ('first_name', 'last_name')
+
+
+class RouteAdmin(admin.ModelAdmin):
+    empty_value_display = 'N/A'
+    list_display = ('user', 'route_key', 'latitude', 'longitude', 'field_type')
+    search_fields = ('user', 'route_key', 'latitude', 'longitude')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Route, RouteAdmin)
