d8c301719886ecfc75a658618e28f48e0a6b0614
Lekhnath Khanal <lekhnath.khanal@logpoint.com>
2016-07-29 00:29:39 +0545

    Some css styles & html content modified

--- route/static/route/js/maptrigger.js
+++ route/static/route/js/maptrigger.js
@@ -5,7 +5,7 @@ $(function() {
         $.get( "/route/getdestination/",'from_filter='+ val, function(data) {
             var to_filter = data.to_filter;
             if (to_filter != null){
-                $("#to_filter").replaceWith('<select id="to_filter"></select>');
+                $("#to_filter").replaceWith('<select class="form-control" id="to_filter"></select>');
                 $('#to_filter').append("<option value=" + to_filter + ">" + to_filter + "</option>");
             }
         });
